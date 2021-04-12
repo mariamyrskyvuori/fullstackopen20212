f66c6e94c10bc021d9628a80d0d866a9363c8595
Maria Myrskyvuori <mariamyrskyvuori@gmail.com>
2021-04-12 11:14:56 +0300

    style

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -10,32 +10,32 @@ const name = process.argv[3]
 const number = process.argv[4]
 
 const url =
-    `mongodb+srv://fullstack2021:${password}@cluster0.ezdss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack2021:${password}@cluster0.ezdss.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({ name, number });
-person._id instanceof mongoose.Types.ObjectId;
+const person = new Person({ name, number })
+person._id instanceof mongoose.Types.ObjectId
 
 
 Person.find({}).then(result => {
-    console.log(`phonebook:`)
-    result.forEach(person => {
+  console.log('phonebook:')
+  result.forEach(person => {
 
-        console.log(`${person.name} ${person.number}`)
-    })
-    mongoose.connection.close()
+    console.log(`${person.name} ${person.number}`)
+  })
+  mongoose.connection.close()
 })
 
 person.save().then(response => {
-    console.log(`added ${response.name} number ${response.number} to phonebook`)
-    mongoose.connection.close()
+  console.log(`added ${response.name} number ${response.number} to phonebook`)
+  mongoose.connection.close()
 })
